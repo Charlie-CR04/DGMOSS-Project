b546b282e0856ef1f77267f1d13f1bce755cec77
Charlie-CR04 <carlosicr171201@gmail.com>
2025-06-18 18:24:40 -0600

    Comienzo de header

---
+++ includes/header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DGMOSS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    
+    <!-- Navegador 
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Icono</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="https://www.gob.mx/salud/dgmoss" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="https://www.gob.mx/salud/dgmoss">DGMOSS</a></li>
+                            <li><a class="dropdown-item" href="https://www.gob.mx/salud/dgmoss">DGMOSS</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+-->
+    <!-- Navegador con logo y botones -->
+    <nav class="navbar bg-body-tertiary">
+        <!-- Logo -->
+        <div class="container-liquid">
+            <button class=btn btn-link>
+                <a class="navbar-brand" href="index.php"></a>
+                <img src="assets/img/Logo_GM.png" alt="Logo DGMOSS" width="200" height="50" class="align">
+            </button>
+        </div>
+        <!-- Nosotros -->
+        <div>
+            <button class="btn btn-secondary">
+                <a href="#">Nosotros</a>
+            </button>
+        </div>
+        <!-- Direcciones -->
+        <div class="dropdown">
+            <button class="btn btn-secundary btn-dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Direcciones
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="https://www.gob.mx/salud/dgmoss">Dirección 1</a></li>
+                <li><a class="dropdown-item" href="https://www.gob.mx/salud/dgmoss">Dirección 2</a></li>
+            </ul>
+        </div>
+        <!-- Múltimedia-->
+        <div class="dropdown">
+            <button class="btn btn-secundary dropdwon-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Múltimedia
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="https://www.gob.mx/salud/dgmoss">Galería</a></li>
+                <li><a class="dropdown-item" href="https://www.gob.mx/salud/dgmoss">Videos</a></li>
+            </ul>
+        </div>
+        <!-- Contacto -->
+        <div>
+            <button class="btn btn-secondary">
+                <a href="#">Contacto</a>
+            </button>
+        </div>
+    </nav>
+
+    
+
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
