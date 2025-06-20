73f2da60a267717b56c0e1d6d7f6b4b9200060d3
Charlie <carlosicr1712@gmail.com>
2025-06-20 12:56:05 -0600

    Actualización footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html lang="es">
-    <body> 
-        <footer class="footer">
-            <nav class="footer-nav">
-                <a href="#enlaces" class="footer-link">Enlaces</a>
-                <a href="#que-es-gobmx" class="footer-link">¿Qué es gob.mx?</a>
-                <a href="#contacto" class="footer-link">Contacto</a>
-            </nav>
-        </footer>
-    </body>
-</html>
+<footer class="footer">
+    <div class="container text-center">
+        <nav class="footer-nav">
+            <div class="col-sm-3">
+                <h4>Enlaces</h4>
+                <a href="#enlaces" class="footer-link">Enlaces y datos</a>
+            </div>
+            <div class="col-sm-3">
+                <h4>¿Qué es gob.mx?</h4>
+                <a href="#enlaces" class="footer-link">Términos y condiciones</a>
+            </div>
+            <div class="col-sm-3">
+                <h4>Contacto</h4>
+                <a href="#enlaces" class="footer-link">Mucha info nuestra</a>
+                <p></p>
+                <h4>Siguenos en</h4>
+                <ul class="list-inline">
+                    <li>
+                        <a href="#RS" class="footer-link">
+                            <img src="/dgmoss-project/assets/img/red_social/facebook.png" alt="Red social facebook" width="35" height="35">
+                        </a>
+                        <a href="#RS" class="footer-link">Facebook</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!--<p class="mt-3 mb-0 text-center">&copy; 2025 DGMOSS - Todos los derechos reservados</p>-->
+    </div>
+</footer>
