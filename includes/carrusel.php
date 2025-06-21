bbb83523c2485646e9ad5e4a8bb23239e41db72f
Charlie <carlosicr1712@gmail.com>
2025-06-20 23:10:49 -0600

    Actualización carrusel

--- includes/carrusel.php
+++ includes/carrusel.php
@@ -1,28 +1,32 @@
-<div id="carrusel" class="carousel slide" data-bs-ride="carousel">
-    <!-- Indicatores -->
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carrusel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carrusel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carrusel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+<div class="bkg_Default p-3">
+    <div class="container p-3 margen-negtop">
+        <div id="carrusel" class="carousel slide" data-bs-ride="carousel">
+            <!-- Indicatores -->
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carrusel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carrusel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carrusel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <!-- Contenedor de slides -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/dgmoss-project/assets/img/img1.jpg" class="d-block w-100" alt="gobmx">
+                </div>
+                <div class="carousel-item">
+                    <img src="/dgmoss-project/assets/img/img2.jpg" class="d-block w-100" alt="gobmx 1">
+                </div>
+                <div class="carousel-item">
+                    <img src="/dgmoss-project/assets/img/img3.jpg" class="d-block w-100" alt="gobmx 2">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carrusel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carrusel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div> 
     </div>
-    <!-- Contenedor de slides -->
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="/dgmoss-project/assets/img/img1.jpg" class="d-block w-100" alt="gobmx">
-        </div>
-        <div class="carousel-item">
-            <img src="/dgmoss-project/assets/img/img2.jpg" class="d-block w-100" alt="gobmx 1">
-        </div>
-        <div class="carousel-item">
-            <img src="/dgmoss-project/assets/img/img3.jpg" class="d-block w-100" alt="gobmx 2">
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carrusel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carrusel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div> 
+</div>
