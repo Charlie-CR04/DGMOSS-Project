15e27f3be682d2421522eccdc55b7d7b54f2a6a7
Charlie <carlosicr1712@gmail.com>
2025-06-27 12:15:21 -0600

    Ajuste Direcciones

--- Index.php
+++ Index.php
@@ -7,13 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Krub:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Lora:ital,wght@0,400..700;1,400..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="preload" href="/dgmoss-project/assets/css/style.css" as="style">
     <link rel="stylesheet" href="/dgmoss-project/assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-    <?php
-        include('includes/header.php');
-    ?>
 </head>
 <body>
     <!--<section class="hero-section">
@@ -23,6 +21,10 @@
         </div>
     </section>-->
 
+    <?php
+        include('includes/header.php');
+    ?>
+
     <section class="hero-video-section">
         <video autoplay muted loop playsinline class="hero-video">
             <source src="/dgmoss-project/assets/video/back1.mp4" type="video/mp4">
@@ -31,28 +33,30 @@
         <div class="hero-video-overlay">
             <h1 class="hero-title">Dirección General de <br> Modernización del Sector Salud</h1>
             <p class="hero-subtitle">La DGMOSS proporciona información basada en la mejor evidencia disponible para una adecuada toma de decisiones <br> en materia de Tecnologías para la salud, en los servicios de salud en México.</p>
+            <p></p>
+            <a href="/dgmoss-project/info-page/nosotros.php" class="btn-hero">Conoce más sobre DGMOSS</a>
         </div>
     </section>
 
-<?php
-    include('includes/direcciones_main.php');
-?>
+    <?php
+        include('includes/direcciones_main.php');
+    ?>
 
-<?php
-    include('includes/colab.php');  
-?>
+    <?php
+        include('includes/colab.php');  
+    ?>
 
-<?php
-    include('includes/documentos.php');
-?>
+    <?php
+        include('includes/documentos.php');
+    ?>
 
-<?php
-    include('includes/redes_sociales.php');
-?>
+    <?php
+        include('includes/redes_sociales.php');
+    ?>
 
-<script src="/dgmoss-project/assets/js/script.js"></script>
+    <script src="/dgmoss-project/assets/js/script.js"></script>
 </body>
-<?php
-    include('includes/footer.php');
-?>
+    <?php
+        include('includes/footer.php');
+    ?>
 </html>
--- includes/direcciones_main.php
+++ includes/direcciones_main.php
@@ -3,34 +3,46 @@
         <h2 class="titulo-dir text-center">DIRECCIONES DGMOSS</h2>
         <div class="row justify-content-center g-4">
             <!--    Direccion 1   -->
-            <div class="col-md-3">
+            <div class="col-md-2.4 contain-cards">
                 <div class="card direccion-card shadow-sm h-100">
                     <p></p>
-                    <h5 class="card-title text-center">Dirección de Políticas de <br> Tecnologías para la Salud</h5>
+                    <h5 class="card-title text-center">Dirección General de Modernización del <br> Sector Salud</h5>
                     <a href="/dgmoss-project/direcciones/direccion1.php" class="btn stretched-link"></a>
-                    <img src="https://www.gob.mx/cms/uploads/structure/main_image/2431/AdrianPacheco_OK.jpg" alt="">
+                    <img src="https://www.gob.mx/cms/uploads/structure/main_image/6180/FOTO_JEPO.jpg" alt="">
                     <div class="text-center">
-                        <p class="text-center">Ing. Adrián Pacheco López</p>
+                        <p class="text-center">Dr. José Enrique <br> Pérez Olguín</p>
                     </div>
                 </div>
             </div>
             <!--    Direccion 2   -->
-            <div class="col-md-3">
+            <div class="col-md-2.4 contain-cards">
                 <div class="card direccion-card shadow-sm h-100">
                     <p></p>
-                    <h5 class="card-title text-center">Dirección de Desarrollo e Integración <br> de Medicina Basada en la Evidencia</h5>
-                    <a href="/dgmoss-project/direcciones/direccion2.php" class="btn stretched-link"></a>
-                    <img src="https://www.gob.mx/cms/uploads/structure/main_image/5901/PamelaDelMoral.jpg" alt="">
+                    <h5 class="card-title text-center">Dirección de Políticas de Tecnologías para la Salud</h5>
+                    <a href="/dgmoss-project/direcciones/direccion1.php" class="btn stretched-link"></a>
+                    <img src="https://www.gob.mx/cms/uploads/structure/main_image/2431/AdrianPacheco_OK.jpg" alt="">
                     <div class="text-center">
-                        <p class="text-center">Dra. Pamela Stephany Guiovanna <br> del Moral Villavicencio</p>
+                        <p class="text-center">Ing. Adrián Pacheco <br> López</p>
                     </div>
                 </div>
             </div>
             <!--    Direccion 3   -->
-            <div class="col-md-3">
+            <div class="col-md-2.4 contain-cards">
                 <div class="card direccion-card shadow-sm h-100">
                     <p></p>
-                    <h5 class="card-title text-center">Dirección de Gestión <br> de Equipo Médico</h5>
+                    <h5 class="card-title text-center">Dirección de Desarrollo e Integración de Medicina Basada en la Evidencia</h5>
+                    <a href="/dgmoss-project/direcciones/direccion2.php" class="btn stretched-link"></a>
+                    <img src="https://www.gob.mx/cms/uploads/structure/main_image/5901/PamelaDelMoral.jpg" alt="">
+                    <div class="text-center">
+                        <p class="text-center">Dra. Pamela Stephany Guiovanna del Moral Villavicencio</p>
+                    </div>
+                </div>
+            </div>
+            <!--    Direccion 4   -->
+            <div class="col-md-2.4 contain-cards">
+                <div class="card direccion-card shadow-sm h-100">
+                    <p></p>
+                    <h5 class="card-title text-center">Dirección de Gestión de Equipo Médico</h5>
                     <a href="/dgmoss-project/direcciones/direccion3.php" class="btn stretched-link"></a>
                     <img src="https://www.gob.mx/cms/uploads/structure/main_image/6181/1.jpg" alt="">
                     <div class="text-center">
@@ -38,15 +50,15 @@
                     </div>
                 </div>
             </div>
-            <!--    Direccion 4   -->
-            <div class="col-md-3">
+            <!--    Direccion 5   -->
+            <div class="col-md-2.4 contain-cards">
                 <div class="card direccion-card shadow-sm h-100">
                     <p></p>
-                    <h5 class="card-title text-center">Dirección de Salud Digital</h5>
+                    <h5 class="card-title text-center">Dirección de Salud <br> Digital</h5>
                     <a href="/dgmoss-project/direcciones/direccion4.php" class="btn stretched-link" class="btn stretched-link"></a>
                     <img src="https://www.gob.mx/cms/uploads/structure/main_image/6107/2.jpg" alt="" >
                     <div class="text-center">
-                        <p class="text-center">Lic. Yibran Alejandro <br> Hernández Montoya</p>
+                        <p class="text-center">Lic. Yibran Alejandro Hernández Montoya</p>
                     </div>
                 </div>
             </div>
