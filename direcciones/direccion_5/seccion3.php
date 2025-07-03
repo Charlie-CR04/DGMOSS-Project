277386cdbee3a91b348319e5f7e251f229e8ccd3
Charlie <carlosicr1712@gmail.com>
2025-07-03 00:29:46 -0600

    Dirección 5 - Sección 3

--- direcciones/direccion_5/seccion3.php
+++ direcciones/direccion_5/seccion3.php
@@ -1,31 +1,71 @@
-<section class="section-tres-DSD">
+<section class="section-dsd-publicaciones py-5">
     <div class="container">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Documentos y publicaciones</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
+        <!-- Filtros y buscador -->
+        <div class="row align-items-center mb-4">
+            <div class="col-md-9">
+                <div class="d-flex flex-wrap gap-3">
+                <!-- Dropdowns -->
+                <div class="dropdown">
+                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                        Docs en Salud Digital
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" target="_blank" href="/dgmoss-project/documentos/dir5/Modelo AMD 2025.pdf">Modelo de Atención Médica a Distancia</a></li>
+                        <li><a class="dropdown-item" href="#">Op2</a></li>
+                        <li><a class="dropdown-item" href="#">Op3</a></li>
                     </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                        Salud Digital en México
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Op1</a></li>
+                        <li><a class="dropdown-item" href="#">Op2</a></li>
+                        <li><a class="dropdown-item" href="#">Op3</a></li>
+                    </ul>
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                        Info de Salud Digital
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Op1</a></li>
+                        <li><a class="dropdown-item" href="#">Op2</a></li>
+                        <li><a class="dropdown-item" href="#">Op3</a></li>
+                    </ul>
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                        Docs Internacionales
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Op1</a></li>
+                        <li><a class="dropdown-item" href="#">Op2</a></li>
+                        <li><a class="dropdown-item" href="#">Op3</a></li>
+                    </ul>
                 </div>
             </div>
-        </nav>
+        </div>
+        <!-- Buscador -->
+        <div class="col-md-3 mt-3 mt-md-0">
+            <input type="text" class="form-control" placeholder="Buscar">
+        </div>
     </div>
+
+    <!-- Publicaciones destacadas -->
+    <div class="row g-4">
+        <div class="col-md-4">
+            <div class="card h-100 shadow-sm">
+                <img src="/dgmoss-project/assets/img/dir1-public/AMD.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Modelo de Atención Médica a Distancia</h5>
+                    <p class="card-text">La atención Médica a Distancia es el conjunto de servicios médicos que se proporcionan
+                        al individuo, con el fin de promover, proteger y restaurar su salud con el apoyo y uso de las TIC.</p>
+                    <a href="/dgmoss-project/documentos/dir5/Modelo AMD 2025.pdf" class="btn btn-outline-primary btn-sm" target="_blank">Ver más</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
 </section>
