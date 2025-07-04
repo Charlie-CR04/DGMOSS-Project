65925c171129f7668bd09ac7106867401b2e21ac
Charlie-CR04 <carlosicr171201@gmail.com>
2025-07-03 18:10:16 -0600

    Actualización Dirección 5

--- direcciones/direccion_5/seccion3.php
+++ direcciones/direccion_5/seccion3.php
@@ -11,23 +11,23 @@
                     </button>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" target="_blank" href="/dgmoss-project/documentos/dir5/Modelo AMD 2025.pdf">Modelo de Atención Médica a Distancia</a></li>
-                        <li><a class="dropdown-item" href="#">Op2</a></li>
-                        <li><a class="dropdown-item" href="#">Op3</a></li>
+                        <li><a class="dropdown-item" target="_blank" href="/dgmoss-project/documentos/dir5/Documentos de Telesalud/Catalogoserviciosportelesalud2025.pdf">Catálogo de servicios por Telesalud</a></li>
+                        <li><a class="dropdown-item" target="_blank" href="/dgmoss-project/documentos/dir5/Documentos de Telesalud/Cedula_InstrumentosJuridicos_Telesalud_Mx2025.pdf">Cédula de Instrumentos Jurídicos aplicables a la práctica de la Telesalud en México</a></li>
+                    </ul>
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                        Boletines
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" target="_blank" href="/dgmoss-project/documentos/dir5/Boletines/Boletin CECOSAMA Pachuca_versión completa.pdf">Boletin CECOSAMA Pachuca</a></li>
+                        <li><a class="dropdown-item" target="_blank" href="/dgmoss-project/documentos/dir5/Boletines/Boletin de Telesalud Roldan.pdf">Etapas en el desarrollo de un servicio de Telemedicina en un Instituto Nacional de Salud en México</a></li>
+                        <li><a class="dropdown-item" target="_blank" href="/dgmoss-project/documentos/dir5/Boletines/Boletin Jalisco_Ignacio Marcial.pdf">Jalisco construye un nuevo camino hacía el fortalecimento del modelo de atención a la salud</a></li>
                     </ul>
                 </div>
                 <div class="dropdown">
                     <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
                         Salud Digital en México
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Op1</a></li>
-                        <li><a class="dropdown-item" href="#">Op2</a></li>
-                        <li><a class="dropdown-item" href="#">Op3</a></li>
-                    </ul>
-                </div>
-                <div class="dropdown">
-                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                        Info de Salud Digital
                     </button>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Op1</a></li>
@@ -55,16 +55,121 @@
 
     <!-- Publicaciones destacadas -->
     <div class="row g-4">
+        <!-- Publicación 1 -->
         <div class="col-md-4">
-            <div class="card h-100 shadow-sm">
-                <img src="/dgmoss-project/assets/img/dir1-public/AMD.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Modelo de Atención Médica a Distancia</h5>
-                    <p class="card-text">La atención Médica a Distancia es el conjunto de servicios médicos que se proporcionan
-                        al individuo, con el fin de promover, proteger y restaurar su salud con el apoyo y uso de las TIC.</p>
-                    <a href="/dgmoss-project/documentos/dir5/Modelo AMD 2025.pdf" class="btn btn-outline-primary btn-sm" target="_blank">Ver más</a>
+            <a href="/dgmoss-project/documentos/dir5/Documentos de Telesalud/Modelo AMD 2025.pdf" target="_blank" style="text-decoration:none; color:inherit;">
+                <div class="card h-100 shadow-sm">
+                    <img src="/dgmoss-project/assets/img/dir5-public/AMD.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Modelo de Atención Médica a Distancia</h5>
+                        <p class="card-text">La atención Médica a Distancia es el conjunto de servicios médicos que se proporcionan
+                            al individuo, con el fin de promover, proteger y restaurar su salud con el apoyo y uso de las TIC.</p>
+                    </div>
                 </div>
-            </div>
+            </a>
+        </div>
+        <!-- Publicación 2 -->
+        <div class="col-md-4">
+            <a href="/dgmoss-project/documentos/dir5/Documentos de Telesalud/Catalogoserviciosportelesalud2025.pdf" target="_blank" style="text-decoration:none; color:inherit;">
+                <div class="card h-100 shadow-sm">
+                    <img src="/dgmoss-project/assets/img/dir5-public/catalogoserviciosportelesalud.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Catálogo de servicios por Telesalud</h5>
+                        <p class="card-text">Instrumento de referencia para unificar criterios, que permitan a los tomadores de decisión, 
+                        en el ambito de la telesalud, comunicarse en los mismos términos.</p>
+                    </div>
+                </div>
+            </a>        
+        </div>
+        <!-- Publicación 3 -->
+        <div class="col-md-4">
+            <a href="/dgmoss-project/documentos/dir5/Documentos de Telesalud/Cedula_InstrumentosJuridicos_Telesalud_Mx2025.pdf" target="_blank" style="text-decoration:none; color:inherit;">
+                <div class="card h-100 shadow-sm">
+                    <img src="/dgmoss-project/assets/img/dir5-public/cedulaInstrumentosTelesalud.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Cédula de Instrumentos Jurídicos aplicables a la práctica de la Telesalud en México.</h5>
+                        <p class="card-text">Los servicios de Telesalud al igual que cualquier otra actividad en el ámbito de la salud, deben apegarse al marco normativo aplicable.</p>
+                    </div>
+                </div>
+            </a>        
+        </div>
+        <!-- Publicación 4 -->
+        <div class="col-md-4">
+            <a href="/dgmoss-project/documentos/dir5/Boletines/Boletin CECOSAMA Pachuca_versión completa.pdf" target="_blank" style="text-decoration:none; color:inherit;">
+                <div class="card h-100 shadow-sm">
+                    <img src="/dgmoss-project/assets/img/dir5-public/CECOSAMA_Pachuca.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Boletin CECOSAMA Pachuca</h5>
+                        <p class="card-text">Protocolo de Atención por Teleconsulta en Adicciones en la Unidad de Especialidad Médica Centro
+                            Comunitario de Salud Mental y Adicciones, UNEME CECOSAMA Pachuca.</p>
+                    </div>
+                </div>
+            </a>        
+        </div>
+        <!-- Publicación 5 -->
+        <div class="col-md-4">
+            <a href="/dgmoss-project/documentos/dir5/Boletines/Boletin de Telesalud Roldan.pdf" target="_blank" style="text-decoration:none; color:inherit;">
+                <div class="card h-100 shadow-sm">
+                    <img src="https://media-public.canva.com/0Y45c/MAFXF80Y45c/1/wm_s.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Etapas en el desarrollo de un servicio de Telemedicina en un Instituto Nacional de Salud en México</h5>
+                        <p class="card-text">Para evitar el peligro de la desatención de sus pacientes se realizaron evaluaciones emergentes de sus 
+                            expedientes con el fin de identificar a los grupos con mayor peligro de descompensaciones. </p>
+                    </div>
+                </div>
+            </a>        
+        </div>
+        <!-- Publicación 6 -->
+        <div class="col-md-4">
+            <a href="/dgmoss-project/documentos/dir5/Boletines/Boletin Jalisco_Ignacio Marcial.pdf" target="_blank" style="text-decoration:none; color:inherit;">
+                <div class="card h-100 shadow-sm">
+                    <img src="/dgmoss-project/assets/img/dir5-public/Jalisco_Ignacio.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Jalisco construye un nuevo camino hacía el fortalecimento del modelo de atención a la salud</h5>
+                        <p class="card-text">Se han hecho distintos esfuerzos por contar con instituciones que promuevan una cultura de innovación y transparencia en el Estado 
+                            de Jalisco para el uso correcto de los recursos.
+                        </p>
+                    </div>
+                </div>
+            </a>        
+        </div>
+        <!-- Publicación 7 -->
+        <div class="col-md-4">
+            <a href="https://iris.paho.org/handle/10665.2/52007" target="_blank" style="text-decoration:none; color:inherit;">
+                <div class="card h-100 shadow-sm">
+                    <img src="https://media-public.canva.com/wVgKE/MAGhmKwVgKE/1/wm_s.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Teleconsulta durante una pandemia</h5>
+                        <p class="card-text">En situaciones donde se declara una pandemia, los sentimientos de ansiedad e 
+                            incertidumbre pueden abrumar a las personas, y los sistemas de salud pueden tener dificultades 
+                            para hacer frente a una demanda exponencial y fuera de control.</p>
+                    </div>
+                </div>
+            </a>        
+        </div>
+        <!-- Publicación 8 -->
+        <div class="col-md-4">
+            <a href="/dgmoss-project/documentos/dir5/" target="_blank" style="text-decoration:none; color:inherit;">
+                <div class="card h-100 shadow-sm">
+                    <img src="/dgmoss-project/assets/img/dir5-public/" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"></h5>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </a>        
+        </div>
+        <!-- Publicación 9 -->
+        <div class="col-md-4">
+            <a href="/dgmoss-project/documentos/dir5/" target="_blank" style="text-decoration:none; color:inherit;">
+                <div class="card h-100 shadow-sm">
+                    <img src="/dgmoss-project/assets/img/dir5-public/" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"></h5>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </a>        
         </div>
     </div>
   </div>
